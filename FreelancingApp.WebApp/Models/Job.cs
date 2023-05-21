ddc07f4d337749f033c3bd0994d79f82543fe4aa
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-22 00:05:40 +0200

    finished entities

--- FreelancingApp.WebApp/Models/Job.cs
+++ FreelancingApp.WebApp/Models/Job.cs
@@ -1,10 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FreelancingApp.WebApp.Models
 {
+    [Table("Jobs")]
     public class Job
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public Currency Currency { get; set; } = null!;
+        [Required]
+        public Company Company { get; set; } = null!;
+        public ICollection<Experience>? RequiredExperience { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime Deadline { get; set; }
     }
 }
--- FreelancingApp.WebApp/Models/Review.cs
+++ FreelancingApp.WebApp/Models/Review.cs
@@ -1,6 +1,14 @@
-﻿namespace FreelancingApp.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreelancingApp.WebApp.Models
 {
+    [Table("Reviews")]
     public class Review
     {
+        public int Id { get; set; }
+        public string Comment { get; set; } = null!;
+        public int Rating { get; set; }
+        public AppUser User { get; set; } = null!;
+        public Job Job { get; set; } = null!;
     }
 }
