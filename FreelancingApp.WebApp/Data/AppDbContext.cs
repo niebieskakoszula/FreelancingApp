4610805aae0435e8b24b1f9b3780d08bbb2bffde
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-22 01:50:41 +0200

    DbContext

--- FreelancingApp.WebApp/Data/AppDbContext.cs
+++ FreelancingApp.WebApp/Data/AppDbContext.cs
@@ -6,5 +6,16 @@ namespace FreelancingApp.WebApp.Data
 {
     public class AppDbContext : IdentityDbContext<AppUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Job> Jobs { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public DbSet<Experience> Experiences { get; set; } = null!;
+        public DbSet<Currency> Currencies { get; set; } = null!;
+        public DbSet<ContactInfo> Contacts { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<Skill> Skills { get; set; } = null!;
     }
 }
