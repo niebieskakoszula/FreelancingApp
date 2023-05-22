41bd71398be02b3956622834d97f4c0c5b143925
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-22 23:42:12 +0200

    view models

---
+++ FreelancingApp.WebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreelancingApp.WebApp.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FreelancingApp.WebApp/ViewModels/Jobs/JobsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FreelancingApp.WebApp.Models;
+
+namespace FreelancingApp.WebApp.ViewModels.Jobs
+{
+    public class JobsViewModel
+    {
+        public List<Job>? Jobs { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
