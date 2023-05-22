3426a6806c75549f53f6ae3bcc2e71dbc1250061
Przemysław Budzisz <62652473+niebieskakoszula@users.noreply.github.com>
2023-05-22 23:43:07 +0200

    added UserRoles

---
+++ FreelancingApp.WebApp/Helpers/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace FreelancingApp.WebApp.Helpers
+{
+    public class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+        public const string Freelancer = "freelancer";
+        public const string CompanyWorker = "companyworker";
+    }
+}
